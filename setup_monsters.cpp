9480ea7014b04b98fc595f051b4c367f65d31466
Elizabeth D <dier2believe@gmail.com>
2019-10-21 19:47:04 -0700

    Create setup_monsters.cpp

---
+++ setup_monsters.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void setupMonsters(int monsterNum, Monster **monsters) {
+    int health;
+    int armor;
+    string name;
+    
+    for (int i = 0; i < monsterNum; i++) {
+        cout << "What is the monster's name? ";
+        cin >> name;
+        cout << "What is " << name << "'s health? ";
+        cin >> health;
+        cout << "What is " << name << "'s armor? ";
+        cin >> armor;
+        
+        *monsters[i]->setValues(health, armor, name);
+    }
+}
