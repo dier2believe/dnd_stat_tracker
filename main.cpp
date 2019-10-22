dbbf32010352ca7e3a4d408ba1893e10ee39b768
Elizabeth D <dier2believe@gmail.com>
2019-10-21 19:46:37 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,22 @@
 #include <string>
 #include "player_class.h"
 #include "monster_class.h"
-#include "setupPlayers.cpp"
+#include "setup_players.cpp"
 using namespace std;
 
 int main() {
     int playerNum;
+    int monsterNum;
     
     cout << "How many players do you have? ";
     cin >> playerNum;
     Player *players = new Player[playerNum];
     setupPlayers(playerNum, &players);
     
+    cout << "How many monsters do you have? ";
+    cin >> monsterNum;
+    Monster *monsters = new Monster[monsterNum];
+    setupMonsters(monsterNum, &monsters);
     return 0;
 }
 
