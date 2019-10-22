9447a9a05a1f66f857e6e6a1ea5aedf4af021561
Elizabeth D <dier2believe@gmail.com>
2019-10-21 19:19:03 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
-/* main.cpp */
+#include <iostream>
+#include <string>
+#include "player_class.h"
+#include "monster_class.h"
+#include "setupPlayers.cpp"
+using namespace std;
+
+int main() {
+    int playerNum;
+    
+    cout << "How many players do you have? ";
+    cin >> playerNum;
+    Player *players = new Player[playerNum];
+    setupPlayers(playerNum, &players);
+    
+    return 0;
+}
+
