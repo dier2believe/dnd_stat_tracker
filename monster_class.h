cc476cd797fddae0e3b36fa90e1dbcd9e5f41086
Elizabeth D <dier2believe@gmail.com>
2019-10-21 18:52:36 -0700

    Update monster_class.h

--- monster_class.h
+++ monster_class.h
@@ -1,1 +1,35 @@
+#ifndef MONSTER_CPP
+#define MONSTER_CPP
+class Monster {
+private:
+    int health;
+    int armor;
+    string name;
+public:
+    Monster(int hea, int arm, string nam);
+    void changeHealth();
+    void changeArmor();
+}
 
+Monster::Monster(int hea, int arm, string nam) {
+    health = hea;
+    armor = arm;
+    name = nam;
+}
+
+void Monster::changeHealth() {
+    int h;
+    cout << name << "'s current health is: " << health << endl;
+    cout << "How much did the health change? ";
+    cin >> h;
+    health += h;
+};
+
+void Monster::changeArmor() {
+    int a;
+    cout << name << "'s current armor is: " << armor << endl;
+    cout << "How much did the armor change? ";
+    cin >> a;
+    armor += a;
+};
+#endif
