5537844ecf6a1b6005639153843819b170af5d9b
xanman0117 <56854786+xanman0117@users.noreply.github.com>
2019-10-24 16:27:45 -0700

    Update monster_class.h

--- monster_class.h
+++ monster_class.h
@@ -6,9 +6,22 @@ using namespace std;
 
 #ifndef MONSTER_CPP
 #define MONSTER_CPP
+class Types {
+private:
+    string typeName;
+public:
+    void Types();
+}
+
+Types::Types() {
+    cout << "What type is the monster? ";
+    cin >> typeName;
+}
+
 class Monster {
 private:
     Types type;
+    
 public:
     Monster(int = 10, int = 10, string = "unknown");
 };
