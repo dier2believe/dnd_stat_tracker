12566afd88fed03f20bcfefa81643d569addac17
xanman0117 <56854786+xanman0117@users.noreply.github.com>
2019-10-24 16:27:16 -0700

    Update setup_players.cpp

--- setup_players.cpp
+++ setup_players.cpp
@@ -3,5 +3,16 @@
 using namespace std;
 
 void setupPlayers(int playerNum, Player **players) {
-    
+    for(int i = 0; i <= playerNum; i++) {
+        int hea;
+        int arm;
+        string nam;
+        cout << "What is the player's name? ";
+        cin >> nam;
+        cout << "How much health does " << nam << " have? ";
+        cin >> hea;
+        cout << "How much armor does " << nam << " have? ";
+        cin >> arm;
+        (*players)[i].setValues(hea, arm, nam);
+    }
 }
