db329d10cf0cafdb1391b50f4b35c0b8ba90d7b7
Elizabeth D <dier2believe@gmail.com>
2019-10-23 19:57:29 -0700

    Create entity.h

---
+++ entity.h
@@ -0,0 +1,42 @@
+/* Liz Dieringer 
+   Xanth Rossnagle */
+#include <iostream>
+#include <string>
+using namespace std;
+
+#ifndef ENTITY_CPP
+#define ENTITY_CPP
+class Entity {
+private:
+    int health;
+    int armor;
+    string name;
+public:
+    ~Entity() {}
+    void setValues(int hea, int arm, string nam);
+    void changeHealth();
+    void changeArmor();
+};
+
+void Entity::setValues(int hea, int arm, string nam) {
+    health = hea;
+    armor = arm;
+    name = nam;
+}
+
+void Entity::changeHealth() {
+    int h;
+    cout << name << "'s current health is: " << health << endl;
+    cout << "How much did the health change? ";
+    cin >> h;
+    health += h;
+}
+
+void Entity::changeArmor() {
+    int a;
+    cout << name << "'s current armor is: " << armor << endl;
+    cout << "How much did the armor change? ";
+    cin >> a;
+    armor += a;
+}
+#endif
