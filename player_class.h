8172e4851b56d66756ccde5d0f4f807e01f851e1
Elizabeth D <dier2believe@gmail.com>
2019-10-23 19:58:07 -0700

    Update player_class.h

--- player_class.h
+++ player_class.h
@@ -1,46 +1,26 @@
+/* Liz Dieringer 
+   Xanth Rossnagle */
 #include <iostream>
 #include <string>
+#include "entity.h"
 using namespace std;
 
 #ifndef PLAYER_CPP
 #define PLAYER_CPP
-class Player {
+class Player : public entitiy {
 private:
-    int health;
-    int armor;
-    string name;
+    int exp;
+    Mastery skills;
 public:
-    Player(int = 100, int = 50, string = "unknown");
-    void setValues(int hea, int arm, string nam);
-    void changeHealth();
-    void changeArmor();
+    Player(int = 10, int = 10, string = "unknown", int = 0);
+    ~Player() {}
 };
 
-Player::Player(int hea, int arm, string nam) {
+Entity::Entity(int hea, int arm, string nam, int ex) {
     health = hea;
     armor = arm;
     name = nam;
+    exp = ex;
 }
 
-void Player::setValues(int hea, int arm, string nam) {
-    health = hea;
-    armor = arm;
-    name = nam;
-}
-
-void Player::changeHealth() {
-    int h;
-    cout << name << "'s current health is: " << health << endl;
-    cout << "How much did the health change? ";
-    cin >> h;
-    health += h;
-}
-
-void Player::changeArmor() {
-    int a;
-    cout << name << "'s current armor is: " << armor << endl;
-    cout << "How much did the armor change? ";
-    cin >> a;
-    armor += a;
-}
 #endif
