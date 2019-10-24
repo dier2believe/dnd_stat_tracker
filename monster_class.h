d69735fba23e1f5c630a4ac9468fbca12dc1a5e9
Elizabeth D <dier2believe@gmail.com>
2019-10-23 20:02:37 -0700

    Update monster_class.h

--- monster_class.h
+++ monster_class.h
@@ -8,40 +8,14 @@ using namespace std;
 #define MONSTER_CPP
 class Monster {
 private:
-    int health;
-    int armor;
-    string name;
+    Types type;
 public:
     Monster(int = 10, int = 10, string = "unknown");
-    void setValues(int hea, int arm, string nam);
-    void changeHealth();
-    void changeArmor();
 };
 
 Monster::Monster(int hea, int arm, string nam) {
     health = hea;
     armor = arm;
     name = nam;
-}
-
-void Monster::setValues(int hea, int arm, string nam) {
-    health = hea;
-    armor = arm;
-    name = nam;
-}
-void Monster::changeHealth() {
-    int h;
-    cout << name << "'s current health is: " << health << endl;
-    cout << "How much did the health change? ";
-    cin >> h;
-    health += h;
-}
-
-void Monster::changeArmor() {
-    int a;
-    cout << name << "'s current armor is: " << armor << endl;
-    cout << "How much did the armor change? ";
-    cin >> a;
-    armor += a;
 }
 #endif
