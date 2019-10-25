7ff3e570a5c47a96b03be2339e4fe5fa5520f8e6
Elizabeth D <dier2believe@gmail.com>
2019-10-24 22:21:14 -0700

    Update monster_class.h

--- monster_class.h
+++ monster_class.h
@@ -2,21 +2,31 @@
    Xanth Rossnagle */
 #include <iostream>
 #include <string>
+#include "entity_class.h"
+#include "types_class.h"
 using namespace std;
 
 #ifndef MONSTER_CPP
 #define MONSTER_CPP
-class Monster {
+class Monster : public Entity {
 private:
     Types type;
-    
 public:
-    Monster(int = 10, int = 10, string = "unknown");
+    Monster(int = 10, int = 10, string = "unknown", string = "unknown");
+    void setValues(int = 10, int = 10, string = "unknown", string = "unknown");
+    ~Monster() {}
 };
 
-Monster::Monster(int hea, int arm, string nam) {
+Monster::Monster(int hea, int arm, string nam, string typeName):type(typeName) {
     health = hea;
     armor = arm;
     name = nam;
+}
+
+void Monster::setValues(int hea, int arm, string nam, string typeName) {
+    health = hea;
+    armor = arm;
+    name = nam;
+    type.setName(typeName);
 }
 #endif
