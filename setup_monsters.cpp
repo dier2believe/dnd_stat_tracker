cd9d7422591753819cee20d2443c7c1410d649e2
Elizabeth D <dier2believe@gmail.com>
2019-10-24 22:22:44 -0700

    Update setup_monsters.cpp

--- setup_monsters.cpp
+++ setup_monsters.cpp
@@ -1,11 +1,15 @@
+/* Xanth Rossnagle
+   Liz Dieringer */
 #include <iostream>
 #include <string>
+#include "monster_class.h"
 using namespace std;
 
 void setupMonsters(int monsterNum, Monster **monsters) {
     int health;
     int armor;
     string name;
+    string typeName;
     
     for (int i = 0; i < monsterNum; i++) {
         cout << "What is the monster's name? ";
@@ -14,7 +18,9 @@ void setupMonsters(int monsterNum, Monster **monsters) {
         cin >> health;
         cout << "What is " << name << "'s armor? ";
         cin >> armor;
+        cout << "What is " << name << "'s type? ";
+        cin >> typeName;
         
-        (*monsters[i]).setValues(health, armor, name);
+        (*monsters)[i].setValues(health, armor, name, typeName);
     }
 }
