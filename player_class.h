4e8ec52e5da9f3261921b575ffb022ea3b09dacf
Elizabeth D <dier2believe@gmail.com>
2019-10-24 22:21:45 -0700

    Update player_class.h

--- player_class.h
+++ player_class.h
@@ -2,25 +2,35 @@
    Xanth Rossnagle */
 #include <iostream>
 #include <string>
-#include "entity.h"
+#include "entity_class.h"
+#include "mastery_class.h"
 using namespace std;
 
 #ifndef PLAYER_CPP
 #define PLAYER_CPP
-class Player : public entitiy {
+class Player : public Entity {
 private:
     int exp;
     Mastery skills;
 public:
-    Player(int = 10, int = 10, string = "unknown", int = 0);
+    Player(int = 10, int = 10, string = "unknown", string = "unknown", int = 0);
+    void setValues(int = 10, int = 10, string = "unknown", string = "unknown", int = 0);
     ~Player() {}
 };
 
-Entity::Entity(int hea, int arm, string nam, int ex) {
+Player::Player(int hea, int arm, string nam, string className, int ex):skills(className) {
     health = hea;
     armor = arm;
     name = nam;
     exp = ex;
+}
+
+void Player::setValues(int hea, int arm, string nam, string className, int ex) {
+    health = hea;
+    armor = arm;
+    name = nam;
+    exp = ex;
+    skills.setName(className);
 }
 
 #endif
