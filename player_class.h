4ab1798d96d94149d89ddaf87aa74f806fcd3b85
Elizabeth D <dier2believe@gmail.com>
2019-10-25 14:56:42 -0700

    Update player_class.h

--- player_class.h
+++ player_class.h
@@ -1,10 +1,6 @@
-/* Liz Dieringer 
-   Xanth Rossnagle */
-#include <iostream>
-#include <string>
+/* Liz Dieringer */
 #include "entity_class.h"
 #include "mastery_class.h"
-using namespace std;
 
 #ifndef PLAYER_CPP
 #define PLAYER_CPP
@@ -14,8 +10,8 @@ private:
     Mastery skills;
 public:
     Player(int = 10, int = 10, string = "unknown", string = "unknown", int = 0);
-    void setValues(int = 10, int = 10, string = "unknown", string = "unknown", int = 0);
     ~Player() {}
+    void setValues(int = 10, int = 10, string = "unknown", string = "unknown", int = 0);
 };
 
 Player::Player(int hea, int arm, string nam, string className, int ex):skills(className) {
@@ -32,5 +28,6 @@ void Player::setValues(int hea, int arm, string nam, string className, int ex) {
     exp = ex;
     skills.setName(className);
 }
+    
 
 #endif
