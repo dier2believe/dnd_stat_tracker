8346dd0c3315d7b9c9c6380c72f85879bdab76ab
Elizabeth D <dier2believe@gmail.com>
2019-10-24 22:23:08 -0700

    Update setup_players.cpp

--- setup_players.cpp
+++ setup_players.cpp
@@ -1,18 +1,26 @@
+/* Liz Dieringer */
 #include <iostream>
 #include <string>
+#include "player_class.h"
+#include "mastery_class.h"
 using namespace std;
 
 void setupPlayers(int playerNum, Player **players) {
-    for(int i = 0; i <= playerNum; i++) {
-        int hea;
-        int arm;
-        string nam;
+    int hea;
+    int arm;
+    string nam;
+    string className;
+        
+    for(int i = 0; i < playerNum; i++) {
         cout << "What is the player's name? ";
         cin >> nam;
         cout << "How much health does " << nam << " have? ";
         cin >> hea;
         cout << "How much armor does " << nam << " have? ";
         cin >> arm;
-        (*players)[i].setValues(hea, arm, nam);
+        cout << "What is " << nam << "'s class? ";
+        cin >> className;
+        
+        (*players)[i].setValues(hea, arm, nam, className, 0);
     }
 }
