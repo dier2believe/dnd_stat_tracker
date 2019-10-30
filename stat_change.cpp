609b03e79b1996dd1f557cc7188079c7e5531f7d
Elizabeth D <dier2believe@gmail.com>
2019-10-30 15:34:04 -0700

    Update stat_change.cpp

--- stat_change.cpp
+++ stat_change.cpp
@@ -1,30 +1,33 @@
-//Nicole Lord
+/* Nicole Lord
+   Liz Dieringer */
+#include "entity_class.h"
 #include "player_class.h"
 #include "monster_class.h"
 #include <vector>
 
-void statChange(int turnNumber, vector **orderArr) {
+void statChange(int turnNumber, vector<Entity> &orderArr) {
     char userInput;
-    cout << "Did " << (*orderArr)[turnNumber].getName() 
-        << " health change? (y) for yes and (n) for no";
+    cout << "Did " << orderArr[turnNumber].getName() 
+        << "'s health change? (y)es or (n)o: ";
     cin >> userInput;
     switch(userInput){
         case 'y':
-            (*orderArr)[turnNumber].changeHealth();
+            orderArr[turnNumber].changeHealth();
             break;
         case 'n':
             cout << "Ok. No changes made." << endl;
             break;
     }
-    cout << "Did " << (*orderArr)[turnNumber].getName() 
-        << " armor change? (y) for yes and (n) for no";
+    cout << "Did " << orderArr[turnNumber].getName() 
+        << "'s armor change? (y)es or (n)o: ";
     cin >> userInput;
     switch(userInput){
         case 'y':
-            (*orderArr)[turnNumber].changeArmor();
+            orderArr[turnNumber].changeArmor();
             break;
         case 'n':
             cout << "Ok. No changes made." << endl;
             break;
     }
+    orderArr[turnNumber].isDeadCheck();
 }
