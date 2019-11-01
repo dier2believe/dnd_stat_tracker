9d03931cfd41a80288c6cedb20c5b932bbb00388
Elizabeth D <dier2believe@gmail.com>
2019-11-01 12:01:13 -0700

    Update is_dead_monsters.cpp

--- is_dead_monsters.cpp
+++ is_dead_monsters.cpp
@@ -5,10 +5,8 @@
 int monstersDead(int monsterNum, Monster *monsters) {
     int i;
     int allDead = monsterNum;
-    cout << endl << endl << "Total monsters : " << allDead << endl;
     for (i = 0; i < monsterNum; i++) {
         allDead -= monsters[i].returnIsDead();
-        cout << "monster dead" << endl << "total monsters: " << allDead << endl;
     }
     return allDead;
 }
