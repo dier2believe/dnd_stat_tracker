4fc23a4241954c4fe3cd57ee8330ba1b57b27c47
Elizabeth D <dier2believe@gmail.com>
2019-11-01 12:01:30 -0700

    Update is_dead_players.cpp

--- is_dead_players.cpp
+++ is_dead_players.cpp
@@ -3,11 +3,8 @@
 
 int playersDead(int playerNum, Player *players) {
     int allDead = playerNum;
-    cout << endl << endl << "Total players : " << allDead << endl;
     for (int i = 0; i < playerNum; i++) {
         allDead -= players[i].returnIsDead();
-        cout << "returnIsDead = " << players[i].returnIsDead() << endl;
-        cout << "player dead" << endl << "total players: " << allDead << endl;
     }
     return allDead;
 }
