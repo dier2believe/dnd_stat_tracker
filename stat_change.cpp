9d9b3a408063065ecf3720225649d149472e1415
lordn54 <56842936+lordn54@users.noreply.github.com>
2019-11-05 12:49:32 -0700

    Update stat_change.cpp

--- stat_change.cpp
+++ stat_change.cpp
@@ -5,29 +5,38 @@
 #include "monster_class.h"
 #include <vector>
 
-void statChange(int turnNumber, vector<Entity> &orderArr) {
+/*This is the function that handles any status changes to the health or armor of an entity*/
+void statChange(int turnNumber, vector<Entity*> &orderArr) {
     char userInput;
-    cout << "Did " << orderArr[turnNumber].getName() 
+    /*Asking if the entity in the order lost/gained health*/
+    cout << "Did " << orderArr[turnNumber]->getName() 
         << "'s health change? (y)es or (n)o: ";
     cin >> userInput;
     switch(userInput){
         case 'y':
-            orderArr[turnNumber].changeHealth();
+            /*If health did change, it calls the changeHealth function in the Entity
+              class*/
+            orderArr[turnNumber]->changeHealth();
             break;
         case 'n':
             cout << "Ok. No changes made." << endl;
             break;
     }
-    cout << "Did " << orderArr[turnNumber].getName() 
+    /*Asking if the entity in the order lost/gained armor*/
+    cout << "Did " << orderArr[turnNumber]->getName() 
         << "'s armor change? (y)es or (n)o: ";
     cin >> userInput;
     switch(userInput){
         case 'y':
-            orderArr[turnNumber].changeArmor();
+            /*If armor did change, it calls the changeHealth function in the Entity
+              class*/
+            orderArr[turnNumber]->changeArmor();
             break;
         case 'n':
             cout << "Ok. No changes made." << endl;
             break;
     }
-    orderArr[turnNumber].isDeadCheck();
+    /*After status changes the function calls the isDeadCheck function to see if 
+      the entity has no health and in turn is dead.*/
+    orderArr[turnNumber]->isDeadCheck();
 }
