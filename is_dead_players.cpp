35be73b6234c66b7b625a3a956653384eb907a72
Elizabeth D <dier2believe@gmail.com>
2019-11-07 12:52:39 -0700

    Update is_dead_players.cpp

--- is_dead_players.cpp
+++ is_dead_players.cpp
@@ -1,10 +1,13 @@
 /* Liz Dieringer */
 #include "player_class.h"
 
+// checks to see how many players are still alive
 int playersDead(int playerNum, Player *players) {
     int allDead = playerNum;
     for (int i = 0; i < playerNum; i++) {
+        // subtracts a player from the rest if they are dead
         allDead -= players[i].returnIsDead();
     }
+    // returns the number of players still alive
     return allDead;
 }
