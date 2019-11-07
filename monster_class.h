bd10c45a0f25ac15ff74edcebefb01b59a3463b0
Elizabeth D <dier2believe@gmail.com>
2019-11-07 13:00:12 -0700

    Update monster_class.h

--- monster_class.h
+++ monster_class.h
@@ -4,6 +4,7 @@
 
 #ifndef MONSTER_CPP
 #define MONSTER_CPP
+// set up the Monster class that inherits from Entity
 class Monster : public Entity {
 private:
     Types type;
@@ -14,16 +15,19 @@ public:
     void setValues(int = 10, int = 10, string = "unknown", string = "unknown");
 };
 
+// Default constructor for monsters with all of its values
 Monster::Monster(int hea, int arm, string nam, string typeName):type(typeName) {
     health = hea;
     armor = arm;
     name = nam;
 }
 
+// get the type name which is private
 string Monster::getType() {
     return type.getName();
 }
 
+// change all fo the values once the monster already exists
 void Monster::setValues(int hea, int arm, string nam, string typeName) {
     health = hea;
     armor = arm;
