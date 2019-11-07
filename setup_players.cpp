822ec76704b87c2739819a241ba9892defd84c29
Elizabeth D <dier2believe@gmail.com>
2019-11-07 12:54:53 -0700

    Update setup_players.cpp

--- setup_players.cpp
+++ setup_players.cpp
@@ -2,12 +2,15 @@
 #include "player_class.h"
 #include "mastery_class.h"
 
+// setup the players if the file input did not work
 void setupPlayers(int playerNum, Player **players) {
+    // set up the variables to keep track of the user input
     int hea;
     int arm;
     string nam;
     string className;
-        
+    
+    // ask the user for each value and create the player
     for(int i = 0; i < playerNum; i++) {
         cout << "What is the player's name? ";
         cin >> nam;
@@ -17,6 +20,7 @@ void setupPlayers(int playerNum, Player **players) {
         cin >> arm;
         cout << "What is " << nam << "'s class? ";
         cin >> className;
+        cout << endl;
         
         (*players)[i].setValues(hea, arm, nam, className, 0);
     }
