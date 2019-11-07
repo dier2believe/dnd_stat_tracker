6d13330d44df2080937966f69b99f3593e724aca
lordn54 <56842936+lordn54@users.noreply.github.com>
2019-11-07 16:21:22 -0700

    Update is_dead_monsters.cpp

--- is_dead_monsters.cpp
+++ is_dead_monsters.cpp
@@ -2,12 +2,12 @@
    Liz Dieringer */
 #include "monster_class.h"
 
-/*This function checks to see how many monsters are still alive.*/
+//This function checks to see how many monsters are still alive.
 int monstersDead(int monsterNum, Monster *monsters) {
     int i;
     int allDead = monsterNum;
     for (i = 0; i < monsterNum; i++) {
-        /*Subtracts the monster from the total if it is dead*/
+        //Subtracts the monster from the total if it is dead
         allDead -= monsters[i].returnIsDead();
     }
     return allDead;
