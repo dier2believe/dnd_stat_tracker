d83693b431c90c6ea03f5d1f36ae53a3a6da4010
Elizabeth D <dier2believe@gmail.com>
2019-11-13 13:35:11 -0700

    Update setup_monsters.cpp

--- setup_monsters.cpp
+++ setup_monsters.cpp
@@ -2,12 +2,15 @@
    Liz Dieringer */
 #include "monster_class.h"
 
+// set up the monsters manually if needed
 void setupMonsters(int monsterNum, Monster **monsters) {
+    // variables to get information from the user
     int health;
     int armor;
     string name;
     string typeName;
     
+    // loop through to get the information for each monster
     for (int i = 0; i < monsterNum; i++) {
         cout << "What is the monster's name? ";
         cin >> name;
@@ -18,6 +21,7 @@ void setupMonsters(int monsterNum, Monster **monsters) {
         cout << "What is " << name << "'s type? ";
         cin >> typeName;
         
+        // set up the monster
         (*monsters)[i].setValues(health, armor, name, typeName);
     }
 }
