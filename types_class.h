ebfd9f8533b3f7ba15545a88f80c3a1dd07e2776
Elizabeth D <dier2believe@gmail.com>
2019-11-13 13:36:17 -0700

    Update types_class.h

--- types_class.h
+++ types_class.h
@@ -6,7 +6,7 @@ using namespace std;
 
 #ifndef TYPES_CPP
 #define TYPES_CPP
-
+// class for the monster type
 class Types {
 private:
     string typeName;
@@ -17,14 +17,17 @@ public:
     string getName();
 };
 
+// constructor
 Types::Types(string name) {
     typeName = name;
 }
 
+// function to set the name
 void Types::setName(string name) {
     typeName = name;
 }
 
+// function to get the name
 string Types::getName() {
     return typeName;
 }
